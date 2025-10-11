22653a8163d2723a1e41a33813260bf7f1995fc0
Raksha05-alt <ramasubburaksha05@gmail.com>
2025-10-11 20:40:32 +0800

    fixing commits

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,9 +4,18 @@ import HomeView from '../views/HomeView.vue'
 import FAQView from '../views/FAQView.vue'
 import ServicesView from '../views/ServicesView.vue'
 import ShipmentView from '../views/ShipmentView.vue'
+<<<<<<< HEAD
 import LoginView from '../views/LoginView.vue'
 import CartView from '../views/CartView.vue'
 import HelpView from '../views/HelpView.vue'
+=======
+import LoginView from '../views/SignUpView.vue'
+import CartView from '../views/CartView.vue'
+import HelpView from '../views/HelpView.vue'
+import SignupForm from '@/views/SignUpView.vue'
+import LoginForm from '@/views/LoginView.vue'
+
+>>>>>>> 8ea627c (B)
 
 export const routes = [
   { path: '/', name: 'home', title: 'Home', component: HomeView },
@@ -16,6 +25,12 @@ export const routes = [
   { path: '/login', name: 'login', title: 'Login', component: LoginView },
   { path: '/cart', name: 'cart', title: 'Cart', component: CartView },
   { path: '/help', name: 'help', title: 'Help', component: HelpView },
+<<<<<<< HEAD
+=======
+  { path: '/signup', component: SignupForm},
+  { path: '/login', component: LoginForm},
+  { path: '/', redirect: '/signup' }
+>>>>>>> 8ea627c (B)
 ]
 
 const router = createRouter({
@@ -24,3 +39,7 @@ const router = createRouter({
 })
 
 export default router
+<<<<<<< HEAD
+=======
+
+>>>>>>> 8ea627c (B)
