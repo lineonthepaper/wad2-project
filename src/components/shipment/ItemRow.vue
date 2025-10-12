90fd33336265ec4454ee59455cafe997977439d8
lineonthepaper <lineonrl@gmail.com>
2025-10-13 00:13:52 +0800

    shipment details layout

---
+++ src/components/shipment/ItemRow.vue
@@ -0,0 +1,85 @@
+<template>
+  <tr>
+    <!-- Item Description -->
+    <td>
+      <input type="text" class="form-control form-control-lg" />
+    </td>
+
+    <!-- Declared Currency -->
+    <td>
+      <div style="width: 8em">
+        <select :id="'declaredCurrency' + rowId" class="form-control p-2 rounded" tabindex="0">
+          <option v-for="currency of currencyData" :key="currency.code" :value="currency.code">
+            {{ currency.code }}
+          </option>
+        </select>
+      </div>
+    </td>
+
+    <!-- Declared Value -->
+    <td>
+      <input type="number" class="form-control form-control-lg" style="width: 5em" />
+    </td>
+
+    <!-- Item Weight -->
+    <td>
+      <input
+        type="number"
+        class="form-control form-control-lg"
+        style="width: 5em"
+        placeholder="in kg"
+      />
+    </td>
+
+    <!-- Item Quantity -->
+    <td>
+      <input type="number" class="form-control form-control-lg" style="width: 4em" />
+    </td>
+  </tr>
+</template>
+
+<style scoped>
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  display: none;
+  margin: 0;
+}
+
+input[type='number'] {
+  -moz-appearance: textfield;
+}
+</style>
+
+<script setup>
+import currencyData from '/json/currencyData.json'
+
+import Choices from 'choices.js'
+
+import '/node_modules/choices.js/public/assets/styles/choices.css'
+
+import { onMounted } from 'vue'
+
+const props = defineProps({
+  rowId: Number,
+})
+
+onMounted(() => {
+  const declaredCurrency = document.querySelector('#declaredCurrency' + props.rowId)
+
+  const currencyChoices = new Choices(declaredCurrency, {
+    placeholder: true,
+    placeholderValue: 'Currency',
+    itemSelectText: '',
+  })
+
+  declaredCurrency.parentElement.className = declaredCurrency.className
+})
+</script>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
--- src/components/shipment/ShipmentDetails.vue
+++ src/components/shipment/ShipmentDetails.vue
@@ -1,1 +1,84 @@
-<template>shipment details</template>
+<template>
+  <div class="container-fluid">
+    <div class="row py-2">
+      <div class="col-lg-4" v-for="ref in refNumbers" :key="ref.name">
+        <label :for="ref.name + 'Input'">
+          <h4 class="text-dark-slate-blue pt-2">
+            {{ ref.title }}
+          </h4>
+        </label>
+        <input
+          type="text"
+          :id="ref.name + 'Input'"
+          class="form-control"
+          v-model="ref.textInput"
+          :placeholder="ref.placeholder"
+        />
+      </div>
+    </div>
+
+    <div class="row py-2">
+      <h4 class="text-dark-slate-blue pt-2">Item Details</h4>
+
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Item Description</th>
+            <th>Declared Currency</th>
+            <th>Declared Value</th>
+            <th>Item Weight</th>
+            <th>Item Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          <ItemRow v-for="i in numRows" :key="i" :row-id="i" />
+        </tbody>
+      </table>
+    </div>
+
+    <div class="row py-2 justify-content-center">
+      <div class="col text-center">
+        <button type="button" class="btn btn-pink" @click="numRows++">Add Item</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import ItemRow from './ItemRow.vue'
+</script>
+
+<script>
+export default {
+  data() {
+    let senderRef = null
+    let senderVAT = null
+    let receiverVAT = null
+    const refNumbers = ref([
+      {
+        title: 'Sender Reference',
+        name: 'senderRef',
+        textInput: senderRef,
+        placeholder: 'optional',
+      },
+      {
+        title: 'Sender VAT Number',
+        name: 'senderVAT',
+        textInput: senderVAT,
+        placeholder: 'optional',
+      },
+      {
+        title: 'Receiver VAT Number',
+        name: 'receiverVAT',
+        textInput: receiverVAT,
+        placeholder: 'optional',
+      },
+    ])
+    return {
+      refNumbers,
+      numRows: 1,
+    }
+  },
+}
+</script>
