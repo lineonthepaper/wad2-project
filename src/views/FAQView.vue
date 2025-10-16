c83056c93a65ff8a18f1a7e4843bac86456b966c
Sheryn <sherynleong.2024@computing.smu.edu.sg>
2025-10-16 21:54:33 +0800

    updated faqview

--- src/views/FAQView.vue
+++ src/views/FAQView.vue
@@ -420,8 +420,7 @@
 </a>
 </template>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js">
 document.getElementById('faqSearch').addEventListener('input', function () {
     const query = this.value.toLowerCase();
     document.querySelectorAll('.accordion-item').forEach(item => {
