3f8b4c5ddc43795b7c68981e8c679924b1e32dba
lineonthepaper <lineonrl@gmail.com>
2025-10-20 21:21:56 +0800

    add confirmation view, after checking out

--- src/router/routes.js
+++ src/router/routes.js
@@ -11,7 +11,7 @@ import LoginForm from '@/views/LoginView.vue'
 import ServicesView from '../views/ServicesView.vue'
 import ServiceDetail from '../views/ServiceDetail.vue'
 import customerDB from '../views/customerDB.vue'
-
+import ConfirmationView from '@/views/ConfirmationView.vue'
 
 export const routes = [
   { path: '/', name: 'home', title: 'Home', component: HomeView },
@@ -21,13 +21,12 @@ export const routes = [
   { path: '/login', name: 'login', title: 'Login', component: LoginView },
   { path: '/cart', name: 'cart', title: 'Cart', component: CartView },
   { path: '/help', name: 'help', title: 'Help', component: HelpView },
+  { path: '/confirmation', name: 'confirmation', title: 'Confirmed!', component: ConfirmationView },
   { path: '/signup', component: SignupForm },
   { path: '/login', component: LoginForm },
   { path: '/service/:id', name: 'ServiceDetail', component: ServiceDetail, props: true },
-  { path: '/CDB', component: customerDB }
-
+  { path: '/CDB', component: customerDB },
 ]
-
 
 const router = createRouter({
   history: createWebHistory(),
---
+++ src/views/ConfirmationView.vue
@@ -0,0 +1,1 @@
+<template>shipment confirmation yay</template>
