76d497c090f4a542524961ee63251a4178a5a185
lineonthepaper <lineonrl@gmail.com>
2025-10-20 21:55:40 +0800

    cart store getters and removeSelected action

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -4,6 +4,35 @@ export const useCartStore = defineStore('cart', {
   state: () => ({
     shipments: [],
   }),
-  getters: {},
-  actions: {},
+  getters: {
+    numSelectedShipments() {
+      let count = 0
+      for (let s of this.shipments) {
+        if (s.selected) {
+          count++
+        }
+      }
+      return count
+    },
+    selectedShipments() {
+      let selectedShipments = []
+      for (let s of this.shipments) {
+        if (s.selected) {
+          selectedShipments.push(s)
+        }
+      }
+      return selectedShipments
+    },
+  },
+  actions: {
+    removeSelected() {
+      let unselectedShipments = []
+      for (let s of this.shipments) {
+        if (!s.selected) {
+          unselectedShipments.push(s)
+        }
+      }
+      this.shipments = unselectedShipments
+    },
+  },
 })
