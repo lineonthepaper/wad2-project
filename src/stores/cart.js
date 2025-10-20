907588d522edcc166548b7599f0688888accda86
lineonthepaper <lineonrl@gmail.com>
2025-10-21 00:08:06 +0800

    sample data for testing

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -2,7 +2,53 @@ import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', {
   state: () => ({
-    shipments: [],
+    shipments: [
+      {
+        sender: {
+          name: 'muh hee ow',
+          phoneCode: null,
+          phoneNumber: null,
+          email: 'singpostproj@gmail.com',
+          line1: 'aow',
+          line2: null,
+          line3: null,
+          city: 'muh hee ow city',
+          state: 'state',
+          postalCode: 'one',
+          country: 'SG',
+        },
+        recipient: {
+          name: 'evil muh hee ow',
+          phoneCode: null,
+          phoneNumber: null,
+          email: 'singpostproj@gmail.com',
+          line1: 'aaa',
+          line2: null,
+          line3: null,
+          city: 'evil city',
+          state: 'evil state',
+          postalCode: 'two',
+          country: 'SG',
+        },
+        service: { name: 'Basic Mail' },
+        items: {
+          1: {
+            costSGD: 1,
+            itemCurrency: 'SGD',
+            itemDescription: 'a',
+            itemQuantity: 1,
+            itemValue: 1,
+            itemWeight: 1,
+          },
+        },
+        refNumbers: {},
+        dimensions: { weight: 1, height: 1, length: 1, width: 1 },
+        complete: true,
+        selected: false,
+        totalCostSGD: 1,
+        type: 'Documents',
+      },
+    ],
   }),
   getters: {
     numSelectedShipments() {
