f2994e6eba4f903d79193dbaeb078cbf324ad136
lineonthepaper <lineonrl@gmail.com>
2025-10-23 00:48:01 +0800

    rename labelSection to labelsSection (and store setup)

--- src/components/confirmation/LabelSection.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="container-fluid">
-    <div class="row py-2"><div class="col">labels!!!</div></div>
-  </div>
-</template>
---
+++ src/components/confirmation/LabelsSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container-fluid">
+    <div class="row py-2">
+      <div class="col">labels!!!</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    const labels = useLabelsStore()
+    return {
+      labels,
+    }
+  },
+}
+</script>
+
+<script setup>
+import { useLabelsStore } from '@/stores/labels'
+
+import { ref } from 'vue'
+</script>
