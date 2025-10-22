8ebccd8eac384d187659840e87cd2def3116761f
lineonthepaper <lineonrl@gmail.com>
2025-10-23 00:49:14 +0800

    update filler text, link placeholder

--- src/components/confirmation/FinalSection.vue
+++ src/components/confirmation/FinalSection.vue
@@ -3,7 +3,9 @@
     <div class="row py-2">
       <div class="col">
         <p>Fluffy Shipping by SingPost has it handled!</p>
-        <p>In the meantime, check out your dashboard for more insights on your shipments.</p>
+        <p>
+          In the meantime, check out your <a>dashboard</a> for insights on your shipments and more!
+        </p>
       </div>
     </div>
   </div>
