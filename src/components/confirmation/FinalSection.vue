6e6dded7082c0ab9cbbddbefc3af78eb1da1f5c8
lineonthepaper <lineonrl@gmail.com>
2025-10-23 00:36:32 +0800

    proper layout for sections, and filler text

--- src/components/confirmation/FinalSection.vue
+++ src/components/confirmation/FinalSection.vue
@@ -1,1 +1,10 @@
-<template>Fluffy Shipping by SingPost has it handled!</template>
+<template>
+  <div class="container-fluid">
+    <div class="row py-2">
+      <div class="col">
+        <p>Fluffy Shipping by SingPost has it handled!</p>
+        <p>In the meantime, check out your dashboard for more insights on your shipments.</p>
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/confirmation/LabelSection.vue
+++ src/components/confirmation/LabelSection.vue
@@ -1,1 +1,5 @@
-<template>label viewer</template>
+<template>
+  <div class="container-fluid">
+    <div class="row py-2"><div class="col">labels!!!</div></div>
+  </div>
+</template>
