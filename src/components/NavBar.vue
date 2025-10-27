533fd49269a7b4cc503ff9d516c53289b71b313d
jess <jessicahoi12345@gmail.com>
2025-10-27 12:50:03 +0800

    halps

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -22,10 +22,10 @@ const handleLogout = () => {
   sessionStorage.removeItem('currentUser')
   isLoggedIn.value = false
   currentUser.value = null
-  
+
   // Dispatch event to notify other components
   window.dispatchEvent(new Event('loginStatusChanged'))
-  
+
   // Redirect to home page
   window.location.href = '/'
 }
@@ -52,7 +52,7 @@ onUnmounted(() => {
         <div class="row align-items-center text-center w-100">
           <div class="col-md-2 col-6 justify-content-center justify-content-sm-start p-2">
             <RouterLink :to="{ name: 'home' }">
-              <img src="../assets/Singapore_Post_Logo.png" alt="Singapore Post Logo" />
+              <img src="../assets/wad2_project_logo.png" alt="Singapore Post Logo" />
             </RouterLink>
           </div>
           <div class="col py-2 text-end">
@@ -101,18 +101,12 @@ onUnmounted(() => {
                   </button>
                   <ul class="dropdown-menu dropdown-menu-end">
                     <li>
-                      <button class="dropdown-item" @click="handleLogout">
-                        Logout
+                      <button class="dropdown-item" @click="$router.push('/CDB')">
+                        Customer Dashboard
                       </button>
                     </li>
-                     <li>
-                      <button class="dropdown-item">
-                        Logout
-                      </button>
-=======
                     <li>
                       <button class="dropdown-item" @click="handleLogout">Logout</button>
->>>>>>> 0319bdebdff2846287b0224e9426a3e004680fa2
                     </li>
                   </ul>
                 </li>
