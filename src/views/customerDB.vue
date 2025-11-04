5a7203c6c8db8ab6e52aeb1f83674766ffa58ccc
jessica <jessicahoi12345@gmail.com>
2025-11-05 00:28:52 +0800

    oops

--- src/views/customerDB.vue
+++ src/views/customerDB.vue
@@ -118,7 +118,7 @@
                 <div class="card-actions">
                   <div class="search-box">
                     <i class="fas fa-search"></i>
-                    <h3></i> Recent Shipments</h3>
+                    <h3> Recent Shipments</h3>
                   </div>
                   <button class="btn-icon" title="Filter">
                     <i class="fas fa-filter"></i>
