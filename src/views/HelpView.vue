f4d81cd824f689fa5204e3dd88f4f8552f98a618
Sheryn <sherynleong.2024@computing.smu.edu.sg>
2025-11-05 10:42:13 +0800

    renamet&cpg

--- src/views/HelpView.vue
+++ src/views/HelpView.vue
@@ -14,7 +14,7 @@
       <hr />
       <div class="row bg-light-pink justify-content-center airplane-header">
         <div class="col-lg-4 col-md-6 col-sm-8 py-2 text-center">
-          <h1 class="jua text-hot-pink">Help</h1>
+          <h1 class="jua text-hot-pink">Terms and Conditions</h1>
         </div>
       </div>
       <hr />
