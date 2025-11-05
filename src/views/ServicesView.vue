540301b2f309c7d9124e513cb558a052f0918557
jess <jessicahoi12345@gmail.com>
2025-11-05 15:16:17 +0800

    halps

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -123,7 +123,7 @@ onMounted(() => {
 <template>
   <div class="catalogue-page">
     <hr />
-    <div class="row bg-light-pink justify-content-center airplane-header">
+    <div class="row justify-content-center airplane-header">
       <div class="col-lg-4 col-md-6 col-sm-8 py-2 text-center">
         <h1 class="jua text-hot-pink">Our Services</h1>
       </div>
