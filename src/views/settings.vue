f65075c21a0f3630914853d173582a34410e2865
jessica <jessicahoi12345@gmail.com>
2025-11-06 03:49:04 +0800

    oops

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -126,7 +126,7 @@ export default {
 </script>
 
 <style scoped>
-
+@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css');
 :root {
   --hot-pink: #ff4275;
   --dark-pink: #ff759e;
